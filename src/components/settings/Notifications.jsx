bc999a43fecbc8ca0ab439ce76c5c516c537d697
Lokesh Verma <lokeshverma8989891@gmail.com>
2024-08-18 13:51:17 +0530

    Everthing done and ready to deploy

---
+++ src/components/settings/Notifications.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import SettingSection from "./SettingSection"
+import { Bell } from "lucide-react"
+import ToggleSwitch from "./ToggleSwitch"
+
+
+const Notifications = () => {
+  const [notifications, setNotifications] = useState({
+    push:true,
+    email:false,
+    sms:true,
+  })
+
+    return (
+    <SettingSection icon={Bell} title = {"Notificatons"}>
+        <ToggleSwitch 
+            Label={"Push Notifications"}
+            isOn={notifications.push}
+            onToggle={()=>setNotifications({...notifications, push:!notifications.push})}
+        />
+        <ToggleSwitch 
+            Label={"Email Notifications"}
+            isOn={notifications.email}
+            onToggle={()=>setNotifications({...notifications, email:!notifications.email})}
+        />
+        <ToggleSwitch 
+            Label={"SMS Notifications"}
+            isOn={notifications.sms}
+            onToggle={()=>setNotifications({...notifications, sms:!notifications.sms})}
+        />
+    </SettingSection>
+  )
+}
+
+export default Notifications
